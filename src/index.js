b5edd54805de00a02c45ee70b3844c0fffcfdd59
Eber Hernández <ebershr@gmail.com>
2021-12-08 18:10:09 -0500

    Configuracion de firebase

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import { FirebaseAppProvider } from 'reactfire';
+import firebaseConfig from './firebaseConfig';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </FirebaseAppProvider>,
   document.getElementById('root')
 );
